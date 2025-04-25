bde5cbf274fd4bf109c2c2e794c0ad37bcb79746
YZ4321 <yusef.ziayi@gibraltarleads.ca>
2025-04-24 20:50:49 -0400

    City Coordinates.js
    Added the coords for North American cities

--- web/content.js
+++ web/content.js
@@ -2,7 +2,60 @@
 export const locations = [
   {name: "New York, USA", latitude: 40.7128, longitude: -74.006},
 ];
-
+export const locations = [
+  {name: "Toronto, Canada", latitude: 43.6532, longitude: -79.3832},
+];
+export const locations = [
+  {name: "Chicago, USA", latitude: 41.8781, longitude: -118.2426},
+];
+ export const locations = [
+   {name: "Los Angeles, USA", latitude: 34.0549, longitude: -87.6298},
+];
+ export const locations = [
+   {name: "Vancouver, Canada", latitude:  49.2827, longitude: -123.1207},
+];
+ export const locations = [
+   {name: "Houston, USA", latitude:  29.7601, longitude: -95.3701},
+];
+ export const locations = [
+   {name: "Dearborn, USA", latitude:  42.3223, longitude: -83.1763},
+];
+ export const locations = [
+   {name: "Philadelphia, USA", latitude:  39.9526, longitude: -75.1652},
+];
+ export const locations = [
+   {name: "Detroit, USA", latitude:  42.3297, longitude: -83.0425},
+];
+ export const locations = [
+   {name: "Washington DC, USA", latitude:  38.9072, longitude: -77.0369},
+];
+ export const locations = [
+   {name: "San Francisco, USA", latitude:  37.7749, longitude: -122.4194},
+];
+ export const locations = [
+   {name: "Montreal, Canada", latitude:  45.5019, longitude: -73.5674},
+];
+ export const locations = [
+   {name: "Surrey, Canada", latitude:  49.1913, longitude: -122.8490},
+];
+ export const locations = [
+   {name: "Halifax, Canada", latitude:  44.6509, longitude: -63.5923},
+];
+ export const locations = [
+   {name: "Barrie, Canada", latitude:  44.3894, longitude: -79.6903},
+];
+ export const locations = [
+   {name: "Mississauga, Canada", latitude:  43.5853, longitude: -79.6450},
+];
+ export const locations = [
+   {name: "Vaughan, Canada", latitude: 43.8262, longitude: -79.5562},
+];
+ export const locations = [
+   {name: "Markham, Canada", latitude: 43.8599, longitude: -79.3350},
+];
+ export const locations = [
+   {name: "Dallas, USA", latitude:  32.7767, longitude: -96.7970},
+];
 export const inspirationalContent = [
   {
     type: "hadith",
