acf523bb6875aacb890d930794f30df8bea49f29
Zaid Mian <zaidmian@gmail.com>
2025-05-01 00:24:30 -0400

    Fixed some compile issues with prayertimes.js

--- web/prayertimes.js
+++ web/prayertimes.js
@@ -1,38 +1,36 @@
-
 export class PrayerTimes {
- 
-  function getPrayerTimes(location) {
-    
-  const dateTime = new Date();
-  const currentYear = dateTime.getFullYear();
-  const currentMonth = dateTime.getMonth() + 1;
-  const currentDay = dateTime.getDate();
+  getPrayerTimes(location) {
+    const dateTime = new Date();
+    const currentYear = dateTime.getFullYear();
+    const currentMonth = dateTime.getMonth() + 1;
+    const currentDay = dateTime.getDate();
 
-  // API URL for Aladhan API
-  const API = `https://api.aladhan.com/v1/timingsByAddress/${currentDay}-${currentMonth}-${currentYear}`?location.name;
+    // API URL for Aladhan API
+    const API = `https://api.aladhan.com/v1/timingsByAddress/${currentDay}-${currentMonth}-${currentYear}?${location.name}`;
 
-  fetch(API)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to grab prayer times");
-      }
-      return response.json();
-    })
-    .then((data) => { //Data of the JSON returned from the API
-      const time = data.data.timings;
-      const prayerTimes = {
-        fajr: time.Fajr,
-        sunrise: time.Sunrise,
-        dhuhr: time.Dhuhr,
-        asr: time.Asr,
-        maghrib: time.Maghrib,
-        isha: time.Isha,
-      };
+    fetch(API)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to grab prayer times");
+        }
+        return response.json();
+      })
+      .then((data) => { //Data of the JSON returned from the API
+        const time = data.data.timings;
+        const prayerTimes = {
+          fajr: time.Fajr,
+          sunrise: time.Sunrise,
+          dhuhr: time.Dhuhr,
+          asr: time.Asr,
+          maghrib: time.Maghrib,
+          isha: time.Isha,
+        };
 
-      console.log(prayerTimes);
-      
-    })
-    .catch((error) => {
-      console.error("Failed to grab prayer times!", error);
-    });
+        console.log(prayerTimes);
+
+      })
+      .catch((error) => {
+        console.error("Failed to grab prayer times!", error);
+      });
+  }
 }
