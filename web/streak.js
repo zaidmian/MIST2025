2042065ec57055db9147513351182ab552903937
windmillburger <hairsense.josuke@gmail.com>
2025-05-05 23:49:15 -0400

    Finished up with streak functions and saving

--- web/streak.js
+++ web/streak.js
@@ -6,6 +6,8 @@ const prayerChecks = [
   document.getElementById('ishaCheck')
 ];
 
+const prayerNames = ['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'];
+
 export function setupStreakHandlers() {
   prayerChecks.forEach((check, idx) => {
     check.addEventListener('click', (e) => {handlePrayerCompletionUpdate(idx, e.target.checked)});
@@ -16,4 +18,53 @@ export function setupStreakHandlers() {
 // completed will be a boolean value indicating whether the indicated prayer is marked completed (true) or not (false)
 export function handlePrayerCompletionUpdate(whichPrayer, completed) {
   console.log(`Prayer completion update: ${whichPrayer} -> ${completed}`);
+  let streakData = JSON.parse(localStorage.getItem("prayerStreak"));
+  
+  if (streakData == null) {
+    streakData = {
+      streak: 0,
+      today: {
+        date: new Date().toDateString(),
+        prayers: {
+          fajr: false,
+          dhuhr: false,
+          asr: false,
+          maghrib: false,
+          isha: false,
+        },
+      },
+    };
+  }
+
+  streakData.today.prayers[prayerNames[whichPrayer]] = completed;
+
+  let prayerCheckPassed = true;
+  
+  for (let prayer in streakData.today.prayers) {
+    if (streakData.today.prayers[prayer] === false) {
+      prayerCheckPassed = false;
+      break;
+    }
+  }
+
+  if (prayerCheckPassed) {
+      const currentDay = new Date().toDateString();
+      if (streakData.today.date !== currentDay) {
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        if (yesterday.toDateString() === streakData.today.date) {
+           streakData.streak += 1;
+        } else {
+          streakData.streak = 1;
+        }
+        streakData.today.date = currentDay;
+      }
+  }
+
+  saveStreak(streakData);
 }
+
+export function saveStreak(data) {
+    localStorage.setItem("prayerStreak", JSON.stringify(data));
+  }
+
